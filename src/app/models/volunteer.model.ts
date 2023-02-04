d3fe680b7a2601b73de53c7587b15ac228fe6a91
Victoria Arbaoui <arbaouivictoria@gmail.com>
2023-02-04 18:07:23 +0100

    model volunteer

--- src/app/models/volunteer.model.ts
+++ src/app/models/volunteer.model.ts
@@ -1,2 +1,11 @@
 export class Volunteer {
+    firstName : string;
+    lastName : string;
+    email : String;
+
+    constructor(firstName : string, lastName : string, email : String) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
