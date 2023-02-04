44c439e7cb77b3d04b9e414c5b2d28c1386217c2
Victoria Arbaoui <arbaouivictoria@gmail.com>
2023-02-04 18:50:53 +0100

    volunteer service

--- src/app/models/volunteer.model.ts
+++ src/app/models/volunteer.model.ts
@@ -1,9 +1,11 @@
 export class Volunteer {
+    id: string;
     firstName : string;
     lastName : string;
     email : String;
 
-    constructor(firstName : string, lastName : string, email : String) {
+    constructor(id : string, firstName : string, lastName : string, email : String) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
--- src/app/services/volunteer.service.ts
+++ src/app/services/volunteer.service.ts
@@ -13,9 +13,44 @@ export class VolunteerService {
       private appService: AppService
   ) { }
 
+  public createVolunteer(volunteer: Volunteer): Observable<Volunteer> {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+
+    return this.appService.http.post<Volunteer>(
+        this.appService.apiUrl + this.dataPath,
+        volunteer,
+        { headers }
+    );
+  }
+
   public get(): Observable<Volunteer> {
     return this.appService.http.get<Volunteer>(this.appService.apiUrl + this.dataPath);
   }
+
+  public getById(id: string): Observable<Volunteer> {
+    return this.appService.http.get<Volunteer>(this.appService.apiUrl + this.dataPath+ '/' +{id});
+  }
+
+  public update(volunteer: Volunteer): Observable<Volunteer> {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+
+    return this.appService.http.put<Volunteer>(
+        this.appService.apiUrl + this.dataPath + '/' + volunteer.id,
+        volunteer,
+        { headers }
+    );
+  }
+
+  public deleteVolunteer(id: string): Observable<{}> {
+    return this.appService.http.delete(this.appService.apiUrl + this.dataPath+ '/' +{id});
+  }
+
 }
 
 
