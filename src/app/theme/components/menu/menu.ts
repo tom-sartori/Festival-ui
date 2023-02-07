eea181c8f9eff8c5bd5862386413372268f35b46
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-02-07 14:41:39 +0100

    Add menu.

--- src/app/theme/components/menu/menu.ts
+++ src/app/theme/components/menu/menu.ts
@@ -2,12 +2,15 @@ import {Menu} from './menu.model';
 
 export const horizontalMenuItems = [
 	new Menu(1, 'HOME.NAV', '/', null, null, false, 0),
-	new Menu(2, 'PRODUCT.TITLE', '/product', null, null, false, 0),
-	new Menu(10, 'NAV.PAGES', null, null, null, true, 0),
-	new Menu(12, 'NAV.CHEFS', '/chefs', null, null, false, 10),
-	new Menu(13, 'NAV.CHEF', '/chefs/1', null, null, false, 10),
-	new Menu(20, 'ACCOUNT.TITLE', null, null, null, true, 10),
-	new Menu(21, 'LOGIN.TITLE', '/login', null, null, false, 20),
-	new Menu(22, 'REGISTER.TITLE', '/register', null, null, false, 20),
+	new Menu(10, 'GAME.NAV', '/game', null, null, false, 0),
+	new Menu(20, 'ZONE.NAV', '/zone', null, null, false, 0),
+	new Menu(30, 'VOLUNTEER.NAV', '/volunteer', null, null, false, 0),
+	new Menu(40, 'PRODUCT.TITLE', '/product', null, null, false, 0),
+	new Menu(50, 'NAV.PAGES', null, null, null, true, 0),
+	new Menu(51, 'NAV.CHEFS', '/chefs', null, null, false, 50),
+	new Menu(52, 'NAV.CHEF', '/chefs/1', null, null, false, 50),
+	new Menu(53, 'ACCOUNT.TITLE', null, null, null, true, 50),
+	new Menu(54, 'LOGIN.TITLE', '/login', null, null, false, 50),
+	new Menu(55, 'REGISTER.TITLE', '/register', null, null, false, 50),
 	new Menu(90, 'NAV.ADMIN', '/admin', null, null, false, 0)
 ];
