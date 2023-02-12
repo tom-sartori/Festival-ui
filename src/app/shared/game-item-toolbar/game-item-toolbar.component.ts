688fc5ba9362614146af521444b33df4e271b454
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-02-12 17:47:38 +0100

    Game admin management.

--- src/app/shared/game-item-toolbar/game-item-toolbar.component.ts
+++ src/app/shared/game-item-toolbar/game-item-toolbar.component.ts
@@ -1,35 +1,51 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { GameService } from '@services/game.service';
+import { TranslateService } from '@services/translate.service';
 
 @Component({
-  selector: 'app-game-item-toolbar',
-  templateUrl: './game-item-toolbar.component.html',
-  styleUrls: ['./game-item-toolbar.component.scss']
+	selector: 'app-game-item-toolbar',
+	templateUrl: './game-item-toolbar.component.html',
+	styleUrls: ['./game-item-toolbar.component.scss']
 })
 export class GameItemToolbarComponent implements OnInit {
-  @Output() onChangeCategory: EventEmitter<string | null> = new EventEmitter<string | null>();
-  @Output() onSearch: EventEmitter<string> = new EventEmitter<string>();
-  @Output() onChangeCountPerPage: EventEmitter<number> = new EventEmitter<number>();
-  @Output() onChangeViewType: EventEmitter< { viewType: string, viewCol: number } > = new EventEmitter< { viewType: string, viewCol: number } >();
+	@Output() onChangeCategory: EventEmitter<string | null> = new EventEmitter<string | null>();
+	@Output() onSearch: EventEmitter<string> = new EventEmitter<string>();
+	@Output() onChangeCountPerPage: EventEmitter<number> = new EventEmitter<number>();
+	@Output() onChangeViewType: EventEmitter<{ viewType: string; viewCol: number; }>
+		= new EventEmitter<{ viewType: string; viewCol: number }>();
 
-  public categoryList: string[] = ['enfant', 'famille', 'ambiance', 'initié', 'expert'];
+	public categoryList: string[] = [];
 
-  constructor() { }
+	constructor(public gameService: GameService, public translateService: TranslateService) {
+	}
 
-  ngOnInit(): void { }
+	ngOnInit(): void {
+		this.categoryList = this.gameService.getCategoryList();
 
-  public changeCategory(category: string | null){
-    this.onChangeCategory.emit(category);
-  }
+		this.categoryList.map((category, index) => {
+			this.translateService
+				.getSyncTranslatedValue('GAME.CATEGORY.' + category)
+				.subscribe((translated: string | null) => {
+					if (translated) {
+						this.categoryList[index] = translated;
+					}
+				});
+		});
+	}
 
-  public search(value: string) {
-    this.onSearch.emit(value);
-  }
+	public changeCategory(category: string | null) {
+		this.onChangeCategory.emit(category);
+	}
 
-  public changeCountPerPage(countPerPage: number){
-    this.onChangeCountPerPage.emit(countPerPage);
-  }
+	public search(value: string) {
+		this.onSearch.emit(value);
+	}
 
-  public changeViewType( { viewType, viewCol }: { viewType: string, viewCol: number } ) {
-    this.onChangeViewType.emit( { viewType, viewCol } );
-  }
+	public changeCountPerPage(countPerPage: number) {
+		this.onChangeCountPerPage.emit(countPerPage);
+	}
+
+	public changeViewType({ viewType, viewCol }: { viewType: string; viewCol: number }) {
+		this.onChangeViewType.emit({ viewType, viewCol });
+	}
 }
